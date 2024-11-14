2fa7e3d23ac7c53bee729dafd1d003c8fcdfbc33
79046 <79046@LAPTOP-279GGFSI>
2024-11-14 22:56:22 +0300

    Добавьте файлы проекта.

---
+++ Задачи из методички/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задачи_из_методички
+{
+    struct DataBase
+    {
+        public string name;
+        public string city;
+        public string age;
+        public string pin;
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name} \nГород: {city} \nВозраст: {age} \nПин-код: {pin} \n");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1) Выведите на экран информацию о каждом типе данных в виде:
+            Тип данных – максимальное значение – минимальное значение */
+            Console.WriteLine("Номер 1 \nТип данных - максимальное значение - минимальное значение");
+            Console.WriteLine("byte - {0} - {1}",byte.MaxValue,byte.MinValue);
+            Console.WriteLine("sbyte - {0} - {1}", sbyte.MaxValue, sbyte.MinValue);
+            Console.WriteLine("short - {0} - {1}",short.MaxValue,short.MinValue);
+            Console.WriteLine("ushort - {0} - {1}", ushort.MaxValue, ushort.MinValue);
+            Console.WriteLine("int - {0} - {1}",int.MaxValue,int.MinValue);
+            Console.WriteLine("uint - {0} - {1}", uint.MaxValue, uint.MinValue);
+            Console.WriteLine("long - {0} - {1}",long.MaxValue,long.MinValue);
+            Console.WriteLine("long - {0} - {1}", ulong.MaxValue, ulong.MinValue);
+            Console.WriteLine("float - {0} - {1}",float.MaxValue,float.MinValue);
+            Console.WriteLine("double - {0} - {1}",double.MaxValue,double.MinValue);
+            Console.WriteLine("decimal - {0} - {1}\n",decimal.MaxValue,decimal.MinValue);
+
+            /* 2) Напишите программу, в которой принимаются данные пользователя в виде имени,
+            города, возраста и PIN-кода.Далее сохраните все значение в соответствующей
+            переменной, а затем распечатайте всю информацию в правильном формате */
+            Console.WriteLine("Номер 2");
+            DataBase data;
+            Console.Write("Введите вааше имя: ");
+            data.name = Console.ReadLine();
+            Console.Write("Укажите ваш город: ");
+            data.city = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            data.age = Console.ReadLine();
+            Console.Write("Введите пин-код: ");
+            data.pin = Console.ReadLine();
+            data.Print();
+
+            // 3) Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – на строчные //
+            Console.WriteLine("Номер 3 \nВведите нексолько букв без пробелов");
+            string text = Console.ReadLine();
+            string word = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == text.ToUpper()[i])
+                {
+                    word += text.ToLower()[i];
+                }
+                else
+                {
+                    word += text.ToUpper()[i];
+                }
+            }
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задачи_Тумакова/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачи_Тумакова
+{
+    enum ВУЗ
+    {
+        КГУ,
+        КАИ,
+        КХТИ
+    }
+    enum Bank
+    {
+        Текущий,
+        Сберегательный
+    }
+    struct BankAccountt
+    {
+        public long num;
+        public string name;
+        public float balans;
+        public void Print()
+        {
+            Console.WriteLine("Номер счёта - {0} \nВид счёта - {1} \nБаланс - {2}$ \n",num,name,balans);
+        }
+    }
+    struct Employee
+    {
+        public string name;
+        public ВУЗ university;
+
+        public void Print()
+        {
+            Console.WriteLine("{0} - {1}",name,university);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Упражнение 3.1 Создать перечислимый тип данных отображающий виды банковского
+            счета (текущий и сберегательный). Создать переменную типа перечисления, присвоить ей
+            значение и вывести это значение на печать */
+            Console.WriteLine("Упражнение 3.1");
+            Bank account = Bank.Текущий;
+            Console.WriteLine($"Вид банковского счёта: {account}\n");
+
+            /* Упражнение 3.2 Создать структуру данных, которая хранит информацию о банковском
+            счете – его номер, тип и баланс. Создать переменную такого типа, заполнить структуру
+            значениями и напечатать результат. */
+            Console.WriteLine("Упражнение 3.2");
+            BankAccountt inf;
+            inf.num = 2200220000220022;
+            inf.name = "Текущий";
+            inf.balans = 25005.67f;
+            inf.Print();
+
+            /* Домашнее задание 3.1 Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}. Создать
+            структуру работник с двумя полями: имя, ВУЗ. Заполнить структуру данными и
+            распечатать */
+            Console.WriteLine("Домашнее задание 3.1");
+            ВУЗ Place = ВУЗ.КАИ;
+            Employee dataBase;
+            dataBase.name = "Романов Иван Олегович";
+            dataBase.university = Place; 
+            dataBase.Print();
+            Console.ReadKey();
+            
+            
+            
+
+
+        }
+    }
+}
